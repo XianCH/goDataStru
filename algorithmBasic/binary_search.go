c4fae34b0af2093d1dbf52dec1fa48fcdaa8252f
XianCH <1160698903@qq.com>
2024-05-15 14:01:25 +0800

    binary search

--- algorithmBasic/binary_search.go
+++ algorithmBasic/binary_search.go
@@ -1,18 +1,17 @@
-package algorthmBasic
-
+package algorithmBasic
 
 func BinarySearch(arr []int, target int) int {
-    left, right := 0, len(arr)-1
-    for left <= right {
-        mid := left + (right-left)/2
-        if arr[mid] == target {
-            return mid
-        }
-        if arr[mid] < target {
-            left = mid + 1
-        } else {
-            right = mid - 1
-        }
-    }
-    return -1
+	left, right := 0, len(arr)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if arr[mid] == target {
+			return mid
+		}
+		if arr[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return -1
 }
--- algorithmBasic/binary_search_test.go
+++ algorithmBasic/binary_search_test.go
@@ -1,25 +1,25 @@
-package algorthmBasic
+package algorithmBasic
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestBinarySearch(t *testing.T) {
-    arr := []int{1, 3, 5, 7, 9, 11, 13}
-    tests := []struct {
-        target   int
-        expected int
-    }{
-        {3, 1},   // 3 is at index 1
-        {13, 6},  // 13 is at index 6
-        {6, -1},  // 6 is not in the array
-        {0, -1},  // 0 is not in the array
-    }
+	arr := []int{1, 3, 5, 7, 9, 11, 13}
+	tests := []struct {
+		target   int
+		expected int
+	}{
+		{3, 1},  // 3 is at index 1
+		{13, 6}, // 13 is at index 6
+		{6, -1}, // 6 is not in the array
+		{0, -1}, // 0 is not in the array
+	}
 
-    for _, test := range tests {
-        result := BinarySearch(arr, test.target)
-        if result != test.expected {
-            t.Errorf("BinarySearch(%v, %d) = %d; expected %d", arr, test.target, result, test.expected)
-        }
-    }
+	for _, test := range tests {
+		result := BinarySearch(arr, test.target)
+		if result != test.expected {
+			t.Errorf("BinarySearch(%v, %d) = %d; expected %d", arr, test.target, result, test.expected)
+		}
+	}
 }
